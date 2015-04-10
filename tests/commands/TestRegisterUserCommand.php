c3e1c6afd8e5d139ccc04512f910cc5fe5aeca4d
Manny Salom <manny.salom@gmail.com>
2015-04-10 17:59:16 -0400

    Test RegisterUserCommand PHPUnit

--- tests/commands/TestRegisterUserCommand.php
+++ tests/commands/TestRegisterUserCommand.php
@@ -1,9 +1,31 @@
-<?php 
+<?php
+
+use App\Commands\RegisterMemberCommand; 
 
 class TestRegisterUserCommand
 {
 	public function testHandleReturnsUserAfterRegisteringSuccessfully()
 	{
+		$handler = new RegisterMemberCommand(
+		'jose',
+		'rodriges',
+		'jose@gmail.com',
+		'password',
+		'password',
+		'M',
+		12,
+		06,
+		1980,
+		'http://images/image1.jpg',
+		'http://images/image1.jpg',
+		'12/06/1980'
+		);
+
+		$user = $handler->handle();
+
+		$this->assertInstanceOf('App\User', $user);
 
 	}
 }
+
+
