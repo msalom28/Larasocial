ece4af2e89fecad40f33a14b66f8116e777561c1
Manny Salom <manny.salom@gmail.com>
2015-04-26 15:03:25 -0400

    prepare for production

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 	$('.welcome-alert').fadeIn(300).delay(3500).fadeOut(300);
 
 	//Register and Connect to websocket
-	var socket = io.connect('http://larasocial.info:1337');
+	var socket = io.connect('http://45.55.233.57:1337');
             
     socket.emit('register', {'userId': userId});
 
